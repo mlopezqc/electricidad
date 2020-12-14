873d4126b9b4883fc6fdd2d0c5017a35921a1b37
Mauricio Lopez <mlopezqc@gmail.com>
2020-12-13 22:00:39 -0500

    Formats code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 import React, { useState, useEffect } from "react";
 
 import Jumbotron from "react-bootstrap/Jumbotron";
@@ -8,7 +8,7 @@ import FormControl from "react-bootstrap/FormControl";
 import Table from "react-bootstrap/Table";
 
 const Calculator = () => {
-  const [kwh, setKwh] = useState('');
+  const [kwh, setKwh] = useState("");
   const [cup, setCup] = useState(0);
   const [cupOld, setCupOld] = useState(0);
   const [usd, setUsd] = useState(0);
@@ -28,9 +28,9 @@ const Calculator = () => {
     const pricesOld = [
       [100, 0.09],
       [50, 0.3],
-      [50, 0.40],
-      [50, 0.60],
-      [50, 0.80],
+      [50, 0.4],
+      [50, 0.6],
+      [50, 0.8],
       [50, 1.5],
       [150, 1.8],
       [500, 2],
@@ -49,17 +49,17 @@ const Calculator = () => {
         kWh -= price[0];
       }
     };
-    if ((!isNaN(kwh)) && (kwh < 1000000)) {
+    if (!isNaN(kwh) && kwh < 1000000) {
       setCup(calculateCup(kwh, prices));
       setCupOld(calculateCup(kwh, pricesOld));
-    };
+    }
   }, [kwh]);
   useEffect(() => {
     setUsd(cup / 24);
   }, [cup]);
   return (
     <div className="p-3 m-2 text-center">
-      <InputGroup >
+      <InputGroup>
         <FormControl
           id="kWh"
           value={kwh}
@@ -73,36 +73,36 @@ const Calculator = () => {
         </InputGroup.Append>
       </InputGroup>
       <Container className="p-3 my-3 oldPrice rounded">
-      <h3 >Antes de 01/01/2021</h3>
-      <Table striped borderless hover className="text-center">
-        <thead>
-          <tr>
-            <th>CUP</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>${cupOld.toFixed(2)}</td>
-          </tr>
-        </tbody>
-      </Table>
+        <h3>Antes de 01/01/2021</h3>
+        <Table striped borderless hover className="text-center">
+          <thead>
+            <tr>
+              <th>CUP</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${cupOld.toFixed(2)}</td>
+            </tr>
+          </tbody>
+        </Table>
       </Container>
       <Container className="p-3 my-3 newPrice rounded">
-      <h3 className="m-2">Después de 01/01/2021</h3>
-      <Table striped borderless hover className="text-center">
-        <thead>
-          <tr>
-            <th>CUP</th>
-            <th>USD</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>${cup.toFixed(2)}</td>
-            <td>${usd.toFixed(2)}</td>
-          </tr>
-        </tbody>
-      </Table>
+        <h3 className="m-2">Después de 01/01/2021</h3>
+        <Table striped borderless hover className="text-center">
+          <thead>
+            <tr>
+              <th>CUP</th>
+              <th>USD</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${cup.toFixed(2)}</td>
+              <td>${usd.toFixed(2)}</td>
+            </tr>
+          </tbody>
+        </Table>
       </Container>
     </div>
   );
@@ -112,7 +112,9 @@ function App() {
   return (
     <Container>
       <Jumbotron className="p-3 m-2 bg-primary text-light">
-        <h1 className="header text-center">Calcule su factura eléctrica en Cuba</h1>
+        <h1 className="header text-center">
+          Calcule su factura eléctrica en Cuba
+        </h1>
       </Jumbotron>
       <Calculator />
     </Container>
